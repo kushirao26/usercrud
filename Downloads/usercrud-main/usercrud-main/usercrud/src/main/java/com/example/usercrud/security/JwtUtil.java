ebb86196c7d2acbe0e88c0aff0eac6e236860d35
kushirao26 <kushirao264@gmail.com>
2026-04-16 17:32:31 +0530

    Added the token authorization code

--- Downloads/usercrud-main/usercrud-main/usercrud/src/main/java/com/example/usercrud/security/JwtUtil.java
+++ Downloads/usercrud-main/usercrud-main/usercrud/src/main/java/com/example/usercrud/security/JwtUtil.java
@@ -1,25 +1,34 @@
 package com.example.usercrud.security;
+
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
-import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtil {
+	
+    @Value("${jwt.secret}")
+    private String secretKey;
 
-    private static final String SECRET_KEY =
-            "mysecretkeymysecretkeymysecretkey123456";
-
-    private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    private Key key;
+    
+    @PostConstruct
+    public void init() {
+        key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
 
     public String generateToken(String username) {
         return Jwts.builder()
-                .setSubject(username)
+                .setSubject(username) 
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
-                .signWith(key)
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) 
+                .signWith(key) 
                 .compact();
     }
 
@@ -37,7 +46,7 @@ public class JwtUtil {
             Jwts.parserBuilder()
                     .setSigningKey(key)
                     .build()
-                    .parseClaimsJws(token);
+                    .parseClaimsJws(token); 
             return true;
         } catch (Exception e) {
             return false;
